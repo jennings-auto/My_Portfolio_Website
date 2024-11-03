bf81ab5c45634784d84888a10c22412ef9bd9a7b
Sanimar-Singh <sanimar.manghera@spit.ac.in>
2024-11-03 12:14:53 +0530

    Save changes before deploying

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sanimar_portfolio/',
+  base: '/My_Portfolio_Website/',
   plugins: [react()],
   assetsInclude: ['**/*.glb']
 })
